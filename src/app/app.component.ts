1a6633171de36c63990087b393af4615ca01e797
Juan Reyes <juanmanuelreyesgarcia816@gmail.com>
2024-10-21 23:57:24 -0500

    Signal 2 create and finished activity 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from "./components/Layout/Menu/menu.component";
+import { FooterComponent } from './components/Layout/Footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MenuComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'primeNg';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { SignalEjemplo1Component } from './components/signal-ejemplo1/signal-ejemplo1.component';
+import { SignalEjemplo2Component } from './components/signal-ejemplo2/signal-ejemplo2.component';
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'signal-ejemplo1'
+    },
+    {
+        path: 'signal-ejemplo1',
+        component: SignalEjemplo1Component
+    },
+    {
+        path: 'signal-ejemplo2',
+        component: SignalEjemplo2Component
+    }
+];
